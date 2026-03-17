9ce1f88b71f741b0005dae3bef6fbad711d92c41
Kristin Yap <kmyap128@gmail.com>
2026-03-17 09:12:45 -0400

    wizard's grasp gets updated now

--- my-react-app/src/components/scenario-ui/options/options.jsx
+++ my-react-app/src/components/scenario-ui/options/options.jsx
@@ -1,10 +1,10 @@
 import './options.css'
 
-export function Options({ options }) {
+export function Options({ options, onSelect }) {
     return (
         <div id="options-container">
             {options.map((option, index) => (
-                <button key={index} className="option">
+                <button key={index} className="option" onClick={() => onSelect(index)}>
                     {option}
                 </button>
             ))}
