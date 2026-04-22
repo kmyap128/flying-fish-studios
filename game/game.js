8867279956ee040427f7cbc513a07b2508e1c548
katie-b9030 <klb9030@rit.edu>
2026-04-22 16:21:40 -0400

    window reload and greedy king options

--- game/game.js
+++ game/game.js
@@ -187,9 +187,11 @@ export class Game {
 
   shuffleOptionKeys(options) {
     const keys = Object.keys(options);
-    for (let i = keys.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [keys[i], keys[j]] = [keys[j], keys[i]];
+    if (this.currentScenario.name !== "The Statue of the Greedy King") {
+      for (let i = keys.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [keys[i], keys[j]] = [keys[j], keys[i]];
+      }
     }
     return keys;
   }
