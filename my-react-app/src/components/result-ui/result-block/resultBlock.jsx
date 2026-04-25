5eb6cc006e0989f450586d203d9ff06fb529578a
Tommy Seternus <tas3788@g.rit.edu>
2026-04-24 22:05:54 -0400

    updated WG bar on results screen

--- my-react-app/src/components/result-ui/result-block/resultBlock.jsx
+++ my-react-app/src/components/result-ui/result-block/resultBlock.jsx
@@ -18,8 +18,8 @@ export function ResultBlock({
   const waddlesUsedPassive = passive?.Waddles == "Waddles_Used"
   const sprigUsedPassive = passive?.Sprig == "Sprig_Used"
 
-  const percent = (wizardsGrasp / 11) * 100;
-  const rounded = Math.round(percent / 10) * 10;
+  const percent = (wizardsGrasp / 15) * 100;
+  const rounded = Math.floor(percent / 10) * 10;
   const clamped = Math.max(0, Math.min(100, rounded));
 
   const meterSrc = `/UI_Assets/TotalWG_Meters/WG_Meter_${clamped}.png`
